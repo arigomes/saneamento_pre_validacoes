c2d692102bdbfa2f7920ee0143a8a7c4d878726a
Arimanoel de Matos Gomes <arimanoel.gomes@betha.com.br>
2025-11-26 16:15:33 -0300

    Ajuste na validação 40

--- FOLHA - Validação - 40.sql
+++ FOLHA - Validação - 40.sql
@@ -12,9 +12,26 @@ having quantidade > 1;
 -- CORREÇÃO
 -- Atualiza os RG's repetidos para nulo, evitando duplicidade
  
-update bethadba.pessoas_fisicas pf1
+update bethadba.pessoas_fisicas
    set rg = null
  where rg is not null
-   and pf1.i_pessoas <> (select min(pf2.i_pessoas)
+   and i_pessoas <> (select min(pf2.i_pessoas)
                            from bethadba.pessoas_fisicas pf2
-                          where pf2.rg = pf1.rg);
+                          where pf2.rg = pessoas_fisicas.rg);
+
+-- Ajusta os historicos para valores repetidos em diferentes pessoas
+select list(distinct i_pessoas) as pess,
+		count(distinct i_pessoas) as pessoas,
+       rg,
+       count(rg) as quantidade
+  from bethadba.hist_pessoas_fis
+ group by rg
+having pessoas > 1;
+
+
+update bethadba.hist_pessoas_fis
+   set rg = null
+ where rg is not null
+   and i_pessoas <> (select min(pf2.i_pessoas)
+                           from bethadba.hist_pessoas_fis pf2
+                          where pf2.rg = hist_pessoas_fis.rg);
