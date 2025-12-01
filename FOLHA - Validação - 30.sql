c4ccd8842cd57f74300392e3e64174b2400bdc43
Arimanoel de Matos Gomes <arimanoel.gomes@betha.com.br>
2025-12-01 14:11:51 -0300

    Ajuste da validação 30

--- FOLHA - Validação - 30.sql
+++ FOLHA - Validação - 30.sql
@@ -80,4 +80,35 @@ delete bethadba.hist_salariais
                                      and s.i_entidades = r.i_entidades
                                      and s.dt_canc_resc is null
                                      and s.dt_reintegracao is null
-                                     and mr.dispensados != 3), ' 23:59:59')
+                                     and mr.dispensados != 3), ' 23:59:59');
+
+-- CORREÇÃO 3
+-- Alternativa para atualização das datas que não foram atualizadas na primeira correção
+begin
+	declare w_i_entidades integer;
+    declare w_competencias timestamp;
+	
+	llloop: for ll as meuloop1 dynamic scroll cursor for
+        select hs.i_funcionarios as w_funcionario,
+		       hs.i_entidades as w_i_entidade,
+		       hs.dt_alteracoes as w_dt_alteracao,
+		       r.dt_rescisao,
+		       STRING(r.dt_rescisao, ' ', substring(hs.dt_alteracoes, 12, 8)) as dt_alteracoes_novo
+		  from bethadba.hist_salariais hs
+		 inner join bethadba.rescisoes r
+		    on (hs.i_funcionarios = r.i_funcionarios
+		   and hs.i_entidades = r.i_entidades)
+		 where hs.dt_alteracoes > STRING((select max(s.dt_rescisao) 
+		                                    from bethadba.rescisoes s 
+		                                    join bethadba.motivos_resc mr
+		                                      on (s.i_motivos_resc = mr.i_motivos_resc)
+		                                   where s.i_funcionarios = r.i_funcionarios 
+		                                     and s.i_entidades = r.i_entidades
+		                                     and s.dt_canc_resc is null
+		                                     and s.dt_reintegracao is null
+		                                     and mr.dispensados != 3), ' 23:59:59')
+		 order by hs.i_funcionarios, hs.dt_alteracoes DESC
+	do		
+		update bethadba.hist_salariais set dt_alteracoes = dt_alteracoes_novo where i_entidades = w_i_entidade and i_funcionarios = w_funcionario and dt_alteracoes = w_dt_alteracao;
+	end for;
+end;                                     
